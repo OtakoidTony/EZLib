4b59f2cd251a1827509c0d0bc58bae63cc924d76
로지꾸 <36785562+OtakoidTony@users.noreply.github.com>
2020-05-04 13:56:23 +0900

    Update ezlib.js

--- ezlib.js
+++ ezlib.js
@@ -13,3 +13,14 @@ Array.prototype.sort_by = function (name) {
 Array.prototype.random = function(){
     return this[Math.floor(Math.random()*this.length)];
 }
+
+Array.prototype.findObjectIndex = function (label, value) {
+    for (var i = 0; i < this.length; i++)
+        if (this[i][label] == value) return i;
+    return -1;
+}
+
+Array.prototype.findObject = function (label, value) {
+    if (this.findObjectIndex(label, value) != -1) return this[this.findObjectIndex(label, value)];
+    else return null;
+}
