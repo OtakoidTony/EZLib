b7840616ef09730899f5f0013d7977804663001d
로지꾸 <36785562+OtakoidTony@users.noreply.github.com>
2020-05-04 13:56:42 +0900

    Update ezlib.js

--- ezlib.js
+++ ezlib.js
@@ -1,13 +1,25 @@
-Array.prototype.sort_by = function (name) {
-    return this.sort(function (a, b) {
-        if (a[name] > b[name]) {
-            return 1;
-        }
-        if (a[name] < b[name]) {
-            return -1;
-        }
-        return 0;
-    })
+Array.prototype.sort_by = function (name, ascending = true) {
+    if (ascending) {
+        return this.sort(function (a, b) {
+            if (a[name] > b[name]) {
+                return 1;
+            }
+            if (a[name] < b[name]) {
+                return -1;
+            }
+            return 0;
+        })
+    } else {
+        return this.sort(function (a, b) {
+            if (a[name] > b[name]) {
+                return -1;
+            }
+            if (a[name] < b[name]) {
+                return 1;
+            }
+            return 0;
+        })
+    }
 }
 
 Array.prototype.random = function(){
